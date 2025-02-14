0fe5419965c9767441cfe04affae764f2bc030b1
Ronit9320 <shahronit16@gmail.com>
2025-02-14 19:01:01 +0530

    plugin changes

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -18,6 +18,8 @@ return {
           color_icons = true,
         }
       })
+      vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
     end,
-  },
+  }
 }
